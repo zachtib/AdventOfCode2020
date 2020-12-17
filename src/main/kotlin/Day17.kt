9e5c4c86bba46a764722d67fc88d6f7f03512555
Zach Tibbitts <ztibbitts@etsy.com>
2020-12-17 10:14:10 -0500

    Day 17

---
+++ src/main/kotlin/Day17.kt
@@ -0,0 +1,121 @@
+import util.part1Result
+import util.part2Result
+
+interface HasNeighbors<T> {
+    fun neighbors(): List<T>
+}
+
+data class Point3D(val x: Int, val y: Int, val z: Int) : HasNeighbors<Point3D> {
+    override fun neighbors(): List<Point3D> {
+        val result = mutableListOf<Point3D>()
+        for (dx in -1..1) {
+            for (dy in -1..1) {
+                for (dz in -1..1) {
+                    if (dx != 0 || dy != 0 || dz != 0) {
+                        result.add(Point3D(x + dx, y + dy, z + dz))
+                    }
+                }
+            }
+        }
+        return result
+    }
+}
+
+data class Point4D(val x: Int, val y: Int, val z: Int, val w: Int) : HasNeighbors<Point4D> {
+    override fun neighbors(): List<Point4D> {
+        val result = mutableListOf<Point4D>()
+        for (dx in -1..1) {
+            for (dy in -1..1) {
+                for (dz in -1..1) {
+                    for (dw in -1..1) {
+                        if (dx != 0 || dy != 0 || dz != 0 || dw != 0) {
+                            result.add(Point4D(x + dx, y + dy, z + dz, w + dw))
+                        }
+                    }
+                }
+            }
+        }
+        return result
+    }
+}
+
+class ConwayCubes<T : HasNeighbors<T>>(initialState: String, create: (Int, Int) -> T) {
+    private val activeCubes = mutableSetOf<T>()
+
+    init {
+        val lines = initialState.lines()
+        for (x in lines.indices) {
+            for (y in lines[x].indices) {
+                if (lines[x][y] == '#') {
+                    activeCubes.add(create(x, y))
+                }
+            }
+        }
+    }
+
+    fun isActive(point: T): Boolean {
+        return point in activeCubes
+    }
+
+    private fun flip(point: T) {
+        if (point in activeCubes) {
+            activeCubes.remove(point)
+        } else {
+            activeCubes.add(point)
+        }
+    }
+
+    private fun allCubes(): Set<T> {
+        return activeCubes + activeCubes.map { it.neighbors() }.flatten()
+    }
+
+    private fun cycle() {
+        val pointsToFlip = mutableListOf<T>()
+        for (point in allCubes()) {
+            val activeNeighbors = point.neighbors().count { it in activeCubes }
+            if (point in activeCubes) {
+                if (activeNeighbors !in 2..3) {
+                    pointsToFlip.add(point)
+                }
+            } else {
+                if (activeNeighbors == 3) {
+                    pointsToFlip.add(point)
+                }
+            }
+        }
+        for (point in pointsToFlip) {
+            flip(point)
+        }
+    }
+
+    fun runCycles(n: Int) {
+        for (i in 1..n) {
+            cycle()
+        }
+    }
+
+    fun countActive(): Int {
+        return activeCubes.size
+    }
+}
+
+fun main() {
+    val initialState = """
+        ######.#
+        ##.###.#
+        #.###.##
+        ..#..###
+        ##.#.#.#
+        ##...##.
+        #.#.##.#
+        .###.###
+    """.trimIndent()
+
+    val cubes = ConwayCubes(initialState) { x, y -> Point3D(x, y, z = 0) }
+    cubes.runCycles(6)
+    cubes.countActive().part1Result()
+
+    val cubes4d = ConwayCubes(initialState) { x, y -> Point4D(x, y, z = 0, w = 0) }
+    cubes4d.runCycles(6)
+    cubes4d.countActive().part2Result()
+}
---
+++ src/test/kotlin/Day17Tests.kt
@@ -0,0 +1,86 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class Day17Tests {
+    @Test
+    fun `test neighbors`() {
+        val point = Point3D(0, 0, 0)
+        val actual = point.neighbors()
+        val expected = listOf(
+                Point3D(-1, -1, -1),
+                Point3D(-1, -1, 0),
+                Point3D(-1, -1, 1),
+
+                Point3D(-1, 0, -1),
+                Point3D(-1, 0, 0),
+                Point3D(-1, 0, 1),
+
+                Point3D(-1, 1, -1),
+                Point3D(-1, 1, 0),
+                Point3D(-1, 1, 1),
+
+                Point3D(0, -1, -1),
+                Point3D(0, -1, 0),
+                Point3D(0, -1, 1),
+
+                Point3D(0, 0, -1),
+                Point3D(0, 0, 1),
+
+                Point3D(0, 1, -1),
+                Point3D(0, 1, 0),
+                Point3D(0, 1, 1),
+
+                Point3D(1, -1, -1),
+                Point3D(1, -1, 0),
+                Point3D(1, -1, 1),
+
+                Point3D(1, 0, -1),
+                Point3D(1, 0, 0),
+                Point3D(1, 0, 1),
+
+                Point3D(1, 1, -1),
+                Point3D(1, 1, 0),
+                Point3D(1, 1, 1),
+        )
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test initialization`() {
+        val input = """
+            .#.
+            ..#
+            ###
+        """.trimIndent()
+        val cubes = ConwayCubes(input) { x, y -> Point3D(x, y, z = 0) }
+
+        assertTrue(cubes.isActive(Point3D(0, 1, 0)))
+        assertFalse(cubes.isActive(Point3D(1, 1, 0)))
+    }
+
+    @Test
+    fun `test startup`() {
+        val input = """
+            .#.
+            ..#
+            ###
+        """.trimIndent()
+        val cubes = ConwayCubes(input) { x, y -> Point3D(x, y, z = 0) }
+        cubes.runCycles(6)
+
+        assertEquals(112, cubes.countActive())
+    }
+
+    @Test
+    fun `test 4d startup`() {
+        val input = """
+            .#.
+            ..#
+            ###
+        """.trimIndent()
+        val cubes = ConwayCubes(input) { x, y -> Point4D(x, y, z = 0, w = 0) }
+        cubes.runCycles(6)
+
+        assertEquals(848, cubes.countActive())
+    }
+}
